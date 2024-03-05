116c5839fdf5345bfdc512c89bf790c34076f8ae
eduAlonso33 <158171889+eduAlonso33@users.noreply.github.com>
2024-03-05 14:17:33 +0100

    Add files via upload

--- src/defaultpackage/MainBanco.java
+++ src/defaultpackage/MainBanco.java
@@ -1,16 +1,11 @@
-package defaultpackage;
+
 public class MainBanco {
 	public static void main(String[] args) {
-		Empleado empleado = new Empleado("E012R","Pedro");
-		Cliente cliente = new Cliente("C2304","Raul",20);
-		System.out.println();
-		System.out.println("Empleado: ");
-		System.out.println(empleado.getCodEmp());
-		System.out.println(empleado.getNombre());
-		System.out.println();
-		System.out.println("Cliente: ");
-		System.out.println(cliente.getCodCliente());
-		System.out.println(cliente.getNombre());
-		System.out.println(cliente.getEdad());
+		Cuenta cuenta = new Cuenta("t549","tr43",534.5);
+		
+		System.out.println("Cuenta: ");
+		System.out.println(cuenta.getCcc());
+		System.out.println(cuenta.getCodCliente());
+		System.out.println(cuenta.getSaldo());
 	}
 }
